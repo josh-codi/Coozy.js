224f713ce30a719eb916d8cc6092f4b062cc8a64
josh-codi <odamejoshua37@gmail.com>
2023-07-09 16:34:40 +0000

    coozy.js stylesheet

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('what is this')
+const observer = new IntersectionObserver((entries)=>{
+    
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate')
+            // entry.target.classList.remove('coozy')
+        }else{
+            entry.target.classList.remove('animate')
+            // entry.target.classList.add('coozy')
+        }
+    })
+})
+
+const hiddenElement = document.querySelectorAll('.coozy')
+hiddenElement.forEach(el=>{
+    console.log(el)
+    observer.observe(el)
+})
